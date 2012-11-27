d8dd28688d82934134d17f4d6ceba0bef09d3fb7
Patrick You <patrickyou2001@gmail.com>
2012-11-27 18:53:51 -0500

    updated gitignored

--- Camera.cs
+++ Camera.cs
@@ -14,14 +14,13 @@ namespace ProjectAlphaIota
     {
         protected float _zoom; // Camera Zoom
         public Matrix _transform; // Matrix Transform
-        public Vector2 _pos; // Camera Position
         protected float _rotation; // Camera Rotation
 
         public Camera()
         {
             _zoom = 1.0f;
             _rotation = 0.0f;
-            _pos = Vector2.Zero;
+            Pos = Vector2.Zero;
         }
         public float Zoom
         {
@@ -34,19 +33,15 @@ namespace ProjectAlphaIota
         // Auxiliary function to move the camera
         public void Move(Vector2 amount)
         {
-            _pos += amount;
+            Pos += amount;
         }
 
         // Get set position
-        public Vector2 Pos
-        {
-            get { return _pos; }
-            set { _pos = value; }
-        }
+        public Vector2 Pos { get; set; }
         public Matrix get_transformation(GraphicsDevice graphicsDevice)
         {
             _transform =       // Thanks to o KB o for this solution
-              Matrix.CreateTranslation(new Vector3(-_pos.X, -_pos.Y, 0)) *
+              Matrix.CreateTranslation(new Vector3(-Pos.X, -Pos.Y, 0)) *
                                          Matrix.CreateRotationZ(Rotation) *
                                          Matrix.CreateScale(new Vector3(Zoom, Zoom, 1)) *
                                          Matrix.CreateTranslation(new Vector3(0,0, 0));
